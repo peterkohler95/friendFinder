6e1c2a597218e834edf1acc84e4077a8768abcb4
Peter K <peterkohler95@gmail.com>
2018-09-11 23:36:46 -0500

    added app folder

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var friendsArray = [
+    {
+        name: 'Pete',
+        photo: 'https://www.google.com/search?q=pete&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiXjqzfzrTdAhUQk1kKHTiCCSAQ_AUICigB&biw=1261&bih=668#imgrc=u0Ftpp_XxEaMdM:',
+        scores: [
+            '1',
+            '5',
+            '5',
+            '1',
+            '5',
+            '5',
+            '1',
+            '1',
+            '1',
+            '5'
+        ]
+    }
+];
+
+module.exports = friendsArray;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,47 @@
+var path = require('path');
+
+var friends = require('../data/friends.js');
+
+
+module.exports = function (app) {
+
+    app.get('/api/friends', function (req, res) {
+        res.json(friends);
+    });
+
+
+    app.post('/api/friends', function (req, res) {
+
+        var userInput = req.body;
+
+
+        var userResponses = userInput.scores;
+
+        var matchName = '';
+        var matchImage = '';
+        var totalDifference = 1000;
+
+
+        for (var i = 0; i < friends.length; i++) {
+
+            var diff = 0;
+            for (var j = 0; j < userResponses.length; j++) {
+                diff += Math.abs(friends[i].scores[j] - userResponses[j]);
+            }
+
+            if (diff < totalDifference) {
+
+
+                totalDifference = diff;
+                matchName = friends[i].name;
+                matchImage = friends[i].photo;
+            }
+        }
+
+        // Add new user
+        friends.push(userInput);
+
+        // Send appropriate response
+        res.json({ status: 'OK', matchName: matchName, matchImage: matchImage });
+    });
+};
